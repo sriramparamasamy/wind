0e993e4fe8d1b14a38250e1d8f7cbd94d9e87860
Sriram <sriramparamasamy3@gmail.com>
2018-04-20 17:49:15 +0530

    small changes

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+function palindrome(str) {
+    var len = str.length;
+    for (var i = 0; i < len/2; i++) {
+      if (str[i] !== str[len - 1 - i]) {
+          return false;
+      }
+    }
+    return true;
+   }
+   palindrome("madam");
+   
